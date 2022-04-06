84fc87ed1490c816e804247d3e5af6dd5cd574c0
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-06 06:51:20 +0600

    added change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,7 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="Footer">
-      Fresh Coffee Beans 2022
-    </footer>
+    null
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,10 @@ import headerImage from "../../assets/header.jpg";
 
 function Header() {
 
-  const style = {
-    backgroundImage: `url(${headerImage})`,
-  };
-
+  
 
   return (
-    <header className="Header" style={style}>
-      <h1>
-        The most coffee beans
-      </h1>
-
-      <p>
-        this is an internet shop
-      </p>
-    </header>
+    null
   );
 }
 
