b0a8b749112c89722c949e595a6331495021d1e6
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-13 15:52:03 +0600

    added changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,7 +18,7 @@ function Header() {
           All for your comfortable!
         </h2>
         <p>
-          We try for you and <br/> we wish that you trust us and <br/> all this builds on the confidence!
+          We try for you and we wish that you trust us and all this builds on the confidence!
         </p>
       </div>
     </header>
