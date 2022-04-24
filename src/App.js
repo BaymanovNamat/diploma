63be638c6da62221182ff323a67db334ad48b547
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-24 19:55:01 +0600

    added Layout components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
 import Allandroid from "./pages/Allandroid";
 import Alliphones from "./pages/Alliphones";
 import Faq from "./pages/Faq";
 import Mainpage from "./pages/Mainpage";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
 function App() {
   return (
     <div className="App">
-      < Nav/>
-
-      <Header/>
-      <div className="route__flex">
-        <div className="route__container">
-        <div className="route__content">
+      
+      <Layout>
         <Routes>
+
           <Route path="/" element={<Mainpage/>}/>
           <Route path="/allandroid" element= {<Allandroid/>}/>
           <Route path="alliphones" element={<Alliphones/>}/>
           <Route path="faq" element={<Faq/>}/>
-      </Routes>
-        </div>
-      </div>
-      </div>
+
+        </Routes>
+
+      </Layout>
 
 
-      <Footer/>
     </div>
   );
 }
